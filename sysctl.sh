deb3506fda4317fc51b5271d836d72fbc6a8fa63
Xgankoa <43083374+xgankoa@users.noreply.github.com>
2019-03-06 13:35:04 +0800

    Update sysctl.sh

--- sysctl.sh
+++ sysctl.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 
-tcpdump src 10.0.0.1 and port 80 -i eth0 -w eth0.pcap
+sysctl -w net.core.somaxconn=1024
+sysctl -w net.ipv4.ip_forward=1
+
+sysctl -w vm.drop_caches=3
+sysctl -w vm.overcommit_memory=1
